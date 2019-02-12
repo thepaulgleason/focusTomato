875ba07b324dabbd2c5aff3bb47c617d1f0f2e76
Benjamin Bassett <benjaminbassett@macbook-air-de-benjamin.local>
2019-02-12 14:14:29 -0800

    Initial Commit

---
+++ focusTomato3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  focusTomato3
+//
+//  Created by Benjamin Bassett  on 2/12/19.
+//  Copyright © 2019 com.benbassett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
